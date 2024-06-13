9a27def4c84735d5f2b6253ee25b3b5a347072d4
Mukusha <sveta.gurinovitch@yandex.ru>
2024-06-13 22:29:06 +0300

    DAY - 1

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -1,20 +1,37 @@
 package com.javarush.telegram;
 
-import com.javarush.telegram.ChatGPTService;
-import com.javarush.telegram.DialogMode;
-import com.javarush.telegram.MultiSessionTelegramBot;
-import com.javarush.telegram.UserInfo;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
-    public static final String TELEGRAM_BOT_NAME = "bot-name"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "bot-token"; //TODO: добавь токен бота в кавычках
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+    static Properties property = new Properties();
+
+    static {
+
+        FileInputStream fis;
+        try {
+            fis = new FileInputStream("src/main/resources/config.properties");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            property.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final String TELEGRAM_BOT_NAME = property.getProperty("bot.name");
+    public static final String TELEGRAM_BOT_TOKEN = property.getProperty("bot.token");
+    public static final String OPEN_AI_TOKEN = property.getProperty("OPEN_AI_TOKEN");
+
 
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
@@ -22,7 +39,21 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
-        //TODO: основной функционал бота будем писать здесь
+        String message = getMessageText();
+
+        if (message.equals("/start")) {
+            sendPhotoMessage("main");
+            String text = loadMessage("main");
+            sendTextMessage(text);
+            return;
+        }
+
+        sendTextMessage("*Привет!*");
+        sendTextMessage("Вы написали " + message);
+
+        sendTextButtonsMessage("Выберите режим работы",
+                "Старт", "start",
+                "Стоп", "stop");
 
     }
 
